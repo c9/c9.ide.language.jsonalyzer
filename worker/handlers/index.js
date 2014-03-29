bfc131ed05c023da99894d0e6344c80d9860f8fd
Lennart Kats <lclkats@gmail.com>
2014-03-29 16:12:32 +0100

    Add a JavaScrript-based tag analyzer for shell scripts
    Mostly since this has better performance and handles non-default extensions

--- worker/handlers/index.js
+++ worker/handlers/index.js
@@ -8,6 +8,7 @@ module.exports = [
     require("plugins/c9.ide.language.jsonalyzer/worker/handlers/jsonalyzer_js"),
     require("plugins/c9.ide.language.jsonalyzer/worker/handlers/jsonalyzer_md"),
     require("plugins/c9.ide.language.jsonalyzer/worker/handlers/jsonalyzer_make"),
+    require("plugins/c9.ide.language.jsonalyzer/worker/handlers/jsonalyzer_sh"),
     require("plugins/c9.ide.language.jsonalyzer/worker/handlers/jsonalyzer_php"),
     require("plugins/c9.ide.language.jsonalyzer/worker/handlers/jsonalyzer_ctags"),
 ];
---
+++ worker/handlers/jsonalyzer_sh.js
@@ -0,0 +1,54 @@
+/**
+ * jsonalyzer Makefile analysis
+ *
+ * @copyright 2013, Ajax.org B.V.
+ * @author Lennart Kats <lennart add c9.io>
+ */
+define(function(require, exports, module) {
+
+var jsonalyzer;
+var PluginBase = require("plugins/c9.ide.language.jsonalyzer/worker/jsonalyzer_base_handler");
+var ctagsUtil = require("plugins/c9.ide.language.jsonalyzer/worker/ctags/ctags_util");
+
+var TAGS = [
+    { regex: /(?:^|\n)\s*function ([A-Za-z0-9_]+)/g, kind: "method" },
+    { regex: /(?:^|\n)\s*([A-Za-z0-9_]+)\(\)/g, kind: "method" },
+    { regex: /(?:^|\n)\s*(\.|source)\s+([A-Za-z0-9_]+)/g, kind: "import" }
+];
+var GUESS_FARGS = false;
+var EXTRACT_DOCS = true;
+var LANGUAGES = ["sh"];
+var EXTENSIONS = ["sh"];
+
+var handler = module.exports = Object.create(PluginBase);
+
+handler.init = function(jsonalyzer_worker) {
+    jsonalyzer = jsonalyzer_worker;
+    jsonalyzer.registerHandler(this, LANGUAGES[0], LANGUAGES, EXTENSIONS);
+};
+
+handler.analyzeCurrent = function(path, doc, ast, options, callback) {
+    if (doc === "")
+        return callback(null, {});
+        
+    if (doc.length > jsonalyzer.getMaxFileSizeSupported())
+        return callback(null, {});
+    
+    var results = {};
+    TAGS.forEach(function(tag) {
+        if (tag.kind === "import")
+            return;
+        ctagsUtil.findMatchingTags(
+            path, doc, tag, GUESS_FARGS, EXTRACT_DOCS, results);
+    });
+    callback(null, { properties: results });
+};
+
+handler.analyzeOthers = handler.analyzeCurrentAll;
+
+handler.findImports = function(path, doc, ast, callback) {
+    callback(null, ctagsUtil.findMatchingOpenFiles(path));
+};
+
+
+});
