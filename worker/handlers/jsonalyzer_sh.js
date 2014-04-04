71fbbddcf3aaa96457a4b6c059041c3b5688d9d7
Lennart Kats <lclkats@gmail.com>
2014-04-04 10:03:08 +0200

    Remove needless addition

--- worker/handlers/jsonalyzer_sh.js
+++ worker/handlers/jsonalyzer_sh.js
@@ -12,7 +12,7 @@ var ctagsUtil = require("plugins/c9.ide.language.jsonalyzer/worker/ctags/ctags_u
 
 var TAGS = [
     { regex: /(?:^|\n)\s*([A-Za-z0-9_]+)\(\)/g, kind: "method" },
-    { regex: /(?:^|\n)\s*(\.|source)\s+([A-Za-z0-9_]+)(?:\S|$)/g, kind: "import" }
+    { regex: /(?:^|\n)\s*(\.|source)\s+([A-Za-z0-9_]+)/g, kind: "import" }
 ];
 var GUESS_FARGS = false;
 var EXTRACT_DOCS = true;
