7261f47b26cc47820dde4dcfeac0f010e8125c36
Fabian Jakobs <fabian@c9.io>
2015-04-15 14:05:15 +0000

    convert tests to mocha

--- worker/ctags/ctags_test.js
+++ worker/ctags/ctags_test.js
@@ -1,7 +1,13 @@
 "use server";
+"use mocha";
 
-require("amd-loader");
+require("c9/inline-mocha")(module);
+if (typeof define === "undefined") {
+    require("amd-loader");
+}
+
 var fs = require("fs");
+var assert = require("assert");
 var ctags = require("./ctags.js");
 
 function parseFile(filenameWithPath) {
@@ -13,21 +19,29 @@ function parseFile(filenameWithPath) {
     
     ctags.FS_createPath("/", path, true, true);
     ctags.FS_createDataFile("/" + path, filename, content, true, false);
-    ctags.CTags_parseFile("/" + filenameWithPath);
+    ctags.CTags_parseFile(filenameWithPath);
 }
 
-function onTagEntry(name, kind, lineNumber, sourceFile, language) {
-    console.log(name + " " + kind + " " + lineNumber + " " + sourceFile + " " + language);
-}
 
-function onParseFileComplete(sourceFile) {
-    console.log("Done: " + sourceFile);
-}
-
-var lang = ctags.CTags_getLanguage("ctags_test.js");
-console.log("Language detected: " + lang);
-
-ctags.CTags_setOnTagEntry(onTagEntry);
-ctags.CTags_setOnParsingCompleted(onParseFileComplete);
-
-parseFile(__dirname + "/ctags_test.js");
+describe(__filename, function() {
+    it("should detect the language", function() {
+        var lang = ctags.CTags_getLanguage("ctags_test.js");
+        assert.equal(lang, "JavaScript");
+    });
+    
+    it("should parse a file", function(done) {
+        function onTagEntry(name, kind, lineNumber, sourceFile, language) {
+            console.log(name + " " + kind + " " + lineNumber + " " + sourceFile + " " + language);
+        }
+        
+        function onParseFileComplete(sourceFile) {
+            assert.equal(sourceFile, __filename);
+            done();
+        }
+        
+        ctags.CTags_setOnTagEntry(onTagEntry);
+        ctags.CTags_setOnParsingCompleted(onParseFileComplete);
+        
+        parseFile(__dirname + "/ctags_test.js");
+    });
+});
