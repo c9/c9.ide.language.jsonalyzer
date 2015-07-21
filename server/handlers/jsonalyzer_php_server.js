f6724ef99df99aabdbfa5bc9609314c9be1b98ba
nightwing <amirjanyan@gmail.com>
2015-07-22 01:14:48 +0400

    fix php linting when global php.ini silences errors

--- server/handlers/jsonalyzer_php_server.js
+++ server/handlers/jsonalyzer_php_server.js
@@ -21,9 +21,19 @@ handler.init = function(options, callback) {
 };
 
 handler.analyzeCurrent = function(path, doc, ast, options, callback) {
+    // TODO use -n to ignore php.ini and set values for `error_reporting` and `short_open`
+    var args = [
+        "-l",
+        "-d", "display_errors=On",
+        "-d", "log_errors=Off"
+    ];
+    
+    if (!doc)
+        args.push(path);
+    
     this.$lint(
         "php",
-        doc ? ["-l"]: ["-l", path],
+        args,
         doc,
         function(err, stdout, stderr) {
             if (err) return callback(err);
@@ -31,7 +41,7 @@ handler.analyzeCurrent = function(path, doc, ast, options, callback) {
             var markers = [];
             
             (stdout + stderr).split("\n").forEach(function(line) {
-                var match = line.match(/(?:Parse error: )(.*?) in (?:.*?) on line (\d+)/);
+                var match = line.match(/(?:Parse|Fatal)(?: error: )(.*?) in (?:.*?) on line (\d+)/);
                 if (!match)
                     return;
                 var message = match[1];
