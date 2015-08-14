0517934898b983c146a8c7a483cb0cb10f95bbc3
Lennart Kats <lclkats@gmail.com>
2015-08-14 11:09:18 +0200

    Revert "Revert "ctags for user defined extensions""

--- worker/file_indexer.js
+++ worker/file_indexer.js
@@ -62,7 +62,9 @@ indexer.analyzeCurrent = function(path, docValue, ast, options, callback) {
         console.log("Warning: did not receive a response for 20 seconds from " + plugin.$source);
     }, 20000);
     
-    var plugin = worker.getHandlerFor(path);
+    var language = worker.language;
+    var plugin = worker.getHandlerFor(path, language);
+    options.language = language;
     return plugin.analyzeCurrent(path, docValue, ast, options, function(err, indexEntry, markers) {
         clearTimeout(watcher);
         if (err) {
