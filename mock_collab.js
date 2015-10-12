8401c240b33363efb02a866c66551777f8864bb7
nightwing <amirjanyan@gmail.com>
2015-10-12 18:23:24 +0400

    fix errors when opening hosted config from standalone

--- mock_collab.js
+++ mock_collab.js
@@ -5,7 +5,7 @@ define(function(require, exports, module) {
     "use strict";
 
     main.consumes = ["Plugin"];
-    main.provides = ["collab", "collab.connect"];
+    main.provides = ["collab", "collab.connect", "collab.workspace"];
     return main;
 
     function main(options, imports, register) {
@@ -13,12 +13,14 @@ define(function(require, exports, module) {
         var plugin = new Plugin("Ajax.org", main.consumes);
 
         plugin.freezePublicAPI({
-            get isMaster() { return true; }
+            get isMaster() { return true; },
+            get isAdmin() { return true; }
         });
         
         register(null, {
             collab: plugin,
-            "collab.connect": plugin
+            "collab.connect": plugin,
+            "collab.workspace": plugin,
         });
     }
 });
