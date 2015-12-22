ba9c8b3b739e5b9370d9379745e10716a65556cc
Lennart Kats <lclkats@gmail.com>
2015-12-22 09:38:18 +0000

    Use memoization and interning to optimize latency

--- server/invoke_helper.js
+++ server/invoke_helper.js
@@ -65,9 +65,26 @@ handler.$doInvoke = function(path, doc, options, callback) {
         options.mode != "tempfile" && doc,
         options,
         function(err, stdout, stderr, originalErr) {
+            if (options.memoStrings) {
+                stdout = doMemoStrings(stdout);
+                stderr = doMemoStrings(stderr);
+            }
+            
             callback(err || originalErr, stdout, stderr, Date.now() - start);
         }
     );
+    
+    function doMemoStrings(string) {
+        try {
+            var json = JSON.parse(string);
+            return JSON.stringify(memoStrings(
+                json, options.memoStrings.dictStart, options.memoStrings.dictLength
+            ));
+        }
+        catch (e) {
+            return string;
+        }
+    }
 };
         
 function getTempFile() {
@@ -76,6 +93,55 @@ function getTempFile() {
         .toString("base64")
         .slice(0, 6)
         .replace(/[+\/]+/g, "");
+}
+
+var dict = [];
+function memoStrings(json, dictStart, dictLength) {
+    dict = dict.slice(dictStart, dictLength);
+    var newDictStart = dictLength;
+    var dictMap = {};
+    dict.forEach(function(value, index) {
+        dictMap["_" + value] = index;
+    });
+    
+    return {
+        json: memoObject(json),
+        dict: dict.slice(newDictStart),
+        dictStart: newDictStart
+    };
+    
+    function memoObject(json) {
+        if (Array.isArray(json))
+            return json.map(memoObject);
+        var result = {};
+        for (var key in json) {
+            var key2 = memoValue(key);
+            var value = json[key];
+            var value2;
+            if (typeof value === "object") {
+                value2 = memoObject(value);
+            }
+            else if (typeof value === "number"
+                || (typeof value === "string" && value.length > 5)) {
+                value2 = memoValue(value);
+            }
+            else {
+                value2 = value;
+            }
+            result[key2] = value2;
+        }
+        return result;
+    }
+            
+    function memoValue(value) {
+        var result = dictMap["_" + value];
+        if (!result) {
+            result = dictLength++;
+            dictMap["_" + value] = result;
+            dict[result] = value;
+        }
+        return result;
+    }
 }
 
 });
